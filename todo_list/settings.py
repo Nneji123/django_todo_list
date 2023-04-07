6e58befaa5943a795a75b0fe4cbda18985e11317
Nneji123 <ifeanyinneji777@gmail.com>
2023-04-07 10:33:06 +0100

    updated settings.py and added allowed hosts

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "django-todo-list-e7b1.onrender.com"]
 
 SITE_ID = 2
 
