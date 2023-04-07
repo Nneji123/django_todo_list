8ac20766502d503240c0e1a75cbf91e2b5b1150d
Nneji123 <ifeanyinneji777@gmail.com>
2023-04-07 10:10:03 +0100

    switching to online hosted postgres db for deployment

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -131,10 +131,21 @@ WSGI_APPLICATION = "todo_list.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': os.environ.get('DB-USER'),
+        'PASSWORD': os.environ.get('DB-PASSWORD'),
+        'HOST': os.environ.get('DB-HOST'),
+        'PORT': os.environ.get('DB-PORT'),
     }
 }
 
